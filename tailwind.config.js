5e78ea9077325d41a416b93ea82e45af258b4a1b
Jahnavi Jagasandram <jjahnavi2505@gmail.com>
2025-08-13 21:40:28 +0530

    installed tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./main/**/*.{html,js,jsx,ts,tsx}",
+    "./**/*.{html,js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
